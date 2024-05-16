55961f7b98f2ec0d7491568662c06ea2baaa5f4e
ps1312 <ps131296@gmail.com>
2024-05-16 09:40:10 -0300

    fix test code structure

--- test/Token.js
+++ test/Token.js
@@ -3,10 +3,9 @@ const { expect } = require("chai");
 describe("Token contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
     const [owner] = await ethers.getSigners();
+    const contract = await ethers.deployContract("Token");
+    const ownerBalance = await contract.balanceOf(owner.address);
 
-    const hardhatToken = await ethers.deployContract("Token");
-
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    expect(await contract.totalSupply()).to.equal(ownerBalance);
   });
 });

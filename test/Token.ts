311d0abb291b2af672cb4812e3afdce658bef35e
ps1312 <ps131296@gmail.com>
2024-05-16 10:14:35 -0300

    refactor first test case to use arrow function

--- test/Token.ts
+++ test/Token.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
+  it("Deployment should assign the total supply of tokens to the owner", async () => {
     const [owner] = await ethers.getSigners();
     const contract = await ethers.deployContract("Token");
     const ownerBalance = await contract.balanceOf(owner.address);

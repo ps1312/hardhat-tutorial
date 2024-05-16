f7891ef0d53f424cfe66f634f743b32eca0bd3f9
ps1312 <ps131296@gmail.com>
2024-05-16 10:23:00 -0300

    add deployTokenFixture helper

--- test/Token.ts
+++ test/Token.ts
@@ -1,18 +1,24 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async () => {
-    const [owner] = await ethers.getSigners();
+  async function deployTokenFixture() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
     const contract = await ethers.deployContract("Token");
+
+    return { contract, owner, addr1, addr2 };
+  }
+
+  it("Deployment should assign the total supply of tokens to the owner", async () => {
+    const { contract, owner } = await loadFixture(deployTokenFixture);
     const ownerBalance = await contract.balanceOf(owner.address);
 
     expect(await contract.totalSupply()).to.equal(ownerBalance);
   });
 
   it("Should allow token transfer between accounts", async () => {
-    const [, addr1, addr2] = await ethers.getSigners();
-    const contract = await ethers.deployContract("Token");
+    const { contract, addr1, addr2 } = await loadFixture(deployTokenFixture);
 
     await contract.transfer(addr1, 50);
     expect(await contract.balanceOf(addr1)).to.equal(50);
